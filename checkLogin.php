8b9f1e41c53d5bd89b458a8f3d38cb4909cb80ba
olbo98 <ob655088@gmail.com>
2019-11-21 13:48:25 +0100

    added session variable for basketID

--- checkLogin.php
+++ checkLogin.php
@@ -11,6 +11,11 @@ $password = $_POST["password"];
 $query = "SELECT * FROM Users WHERE Username ='".$username."' AND Password ='".$password."'";
 $result = $conn->query($query);
 $row = $result->fetch_assoc();
+
+$query = "SELECT basketID FROM Baskets WHERE userID=".$row["userID"];
+$result = $conn->query($query);
+$row2 = $result->fetch_assoc();
+
 if($result->num_rows == 0)
 {
     echo "login failed";
@@ -21,6 +26,7 @@ else
 {
     $_SESSION["username"] = $username;
     $_SESSION["userID"] = $row["userID"];
+    $_SESSION["basketID"] = $row2["basketID"];
     header("Location: http://utbweb.its.ltu.se/~olobou-7/shop/index.php");
 }
 ?>
