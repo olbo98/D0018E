e76df33537bf200d0d3252940dcbae561ba95be0
olbo98 <ob655088@gmail.com>
2019-11-25 10:46:54 +0100

    Add files via upload

--- buypage.php
+++ buypage.php
@@ -21,13 +21,11 @@ if (!$conn) {
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-      
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="basket.css" type="text/css">
+    <link 
 
     <title>QT</title>
   </head>
-  <body>
+  <body class ="text-center">
     <nav class="navbar navbar-expand-xl navbar-dark bg-dark">
 		<a class="navbar-brand" href="index.php" style="font-size: 25px;"><img src="Bilder/QTicon.jpg" width="30" height="30" class="d-inline-block align-top" alt="">  Home</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -35,7 +33,7 @@ if (!$conn) {
 		</button>
 		<div class="collapse navbar-collapse" id="navbarNav">
 		<ul class="navbar-nav">
-			<a class="nav-link" href="moviepage.php" style="font-size: 20px;">Movies</a>
+			<a class="nav-link" href="movies.php" style="font-size: 20px;">Movies</a>
 			<a class="nav-link" href="orders.php" style="font-size: 20px;">Order</a>
 		</ul>
 		</div>
@@ -55,52 +53,17 @@ if (!$conn) {
 		</div>
   
     </nav>
-    
-    
-<div class="container">
-	<table id="cart" class="table table-hover table-condensed">
-    				<thead>
-						<tr>
-							<th style="width:50%">Product</th>
-							<th style="width:10%">Price</th>
-							<th style="width:8%">Quantity</th>
-							<th style="width:22%" class="text-center">Subtotal</th>
-							<th style="width:10%"></th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td data-th="Product">
-								<div class="row">
-									<div class="col-sm-2 hidden-xs"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div>
-									<div class="col-sm-10">
-										<h4 class="nomargin">Product 1</h4>
-										<p>Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet.</p>
-									</div>
-								</div>
-							</td>
-							<td data-th="Price">$1.99</td>
-							<td data-th="Quantity">
-								<input type="number" class="form-control text-center" value="1">
-							</td>
-							<td data-th="Subtotal" class="text-center">1.99</td>
-							<td class="actions" data-th="">
-								<button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>								
-							</td>
-						</tr>
-					</tbody>
-					<tfoot>
-						<tr class="visible-xs">
-						</tr>
-						<tr>
-							<td><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
-							<td colspan="2" class="hidden-xs"></td>
-							<td class="hidden-xs text-center"><strong>Total $1.99</strong></td>
-							<td><a href="#" class="btn btn-success btn-block">Checkout <i class="fa fa-angle-right"></i></a></td>
-						</tr>
-					</tfoot>
-				</table>
-</div>
+     <main role="main" class="inner cover">
+    <div class="center" style="margin-top: 5%;">
+    <img src="https://cdnuploads.aa.com.tr/uploads/Contents/2019/08/07/thumbs_b_c_b6d5f296f9f15bee3163b4ba8cd412cc.jpg?v=172836">
+    <h1 class="cover-heading">Thank you for your order!</h1>
+    <p class="lead">Thank you for your order. Your shipping is being prepared by Quentin himself!</p>
+    <p class="lead">
+         <a href="#" class="btn btn-lg btn-secondary">See your orders</a>
+      <a href="#" class="btn btn-lg btn-secondary">Go back to Startpage</a>
+    </p>
+         </div>
+  </main>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
