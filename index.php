cba09592182b26194f6411f3ac6223a8a8cde161
olbo98 <ob655088@gmail.com>
2019-11-25 11:04:00 +0100

    added css

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if (!$conn) {
       <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-       <link href="startpage.css" rel="stylesheet" type="text/css">
+       <link href="index.css" rel="stylesheet" type="text/css">
 
     <title>QT</title>
       <style>
