8054f0e13bed38c2ebe167bf5dba1d5edaac6073
olbo98 <ob655088@gmail.com>
2019-12-06 09:23:23 +0100

    prepared statemenst on login

--- checkLogin.php
+++ checkLogin.php
@@ -5,11 +5,29 @@ include "functions.php";
 
 $conn = connectToDB();
 
+$query = "SELECT * FROM Users WHERE Username = ? AND Password = ?";
+$stmt = $conn->prepare($query);
+$stmt->bind_param("ss", $username, $password);
 $username = $_POST["username"];
 $password = $_POST["password"];
+$stmt->execute();
+$result = $stmt->get_result();
+if(!($row = $result->fetch_assoc())){
+    session_destroy();
+    header("Location: login.php");
+}else{
+    $query = "SELECT basketID FROM Baskets WHERE userID=".$row["userID"];
+    $result2 = $conn->query($query);
+    $row2 = $result2->fetch_assoc();
+    
+    $_SESSION["username"] = $username;
+    $_SESSION["userID"] = $row["userID"];
+    $_SESSION["basketID"] = $row2["basketID"];
+    header("Location: index.php");
+}
 
-$query = "SELECT * FROM Users WHERE Username ='".$username."' AND Password ='".$password."'";
-$result1 = $conn->query($query);
+
+/*$result1 = $conn->query($query);
 if(!($row = $result1->fetch_assoc())){
     echo "login failed";
     session_destroy();
@@ -23,5 +41,5 @@ if(!($row = $result1->fetch_assoc())){
     $_SESSION["userID"] = $row["userID"];
     $_SESSION["basketID"] = $row2["basketID"];
     header("Location: index.php");
-}
+}*/
 ?>
