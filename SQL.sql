1099a60457020300f3135ba17691682f6be45d3e
olbo98 <ob655088@gmail.com>
2019-12-13 14:33:58 +0100

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -71,13 +71,13 @@ CREATE TABLE Pictures(
     FOREIGN KEY(productID) REFERENCES Products(productID)
 );
 
-INSERT INTO `Products`(`name`, `quantity`, `price`, `description`)
+INSERT INTO `Products`(`name`, `quantity`, `price`, `description`, `rating`)
 VALUES
-    ('Inglourious Bastards',10,3000,'In Nazi-occupied France during World War II, a plan to assassinate Nazi leaders by a group of <br> Jewish U.S. soldiers coincides with a theatre owners vengeful plans for the same.'),
-    ('Kill Bill',10,2000,'After awakening from a four-year coma, a former <br> assassin wreaks vengeance on the team of assassins who betrayed her.'),
-    ('Pulp Fiction',10,1000,'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair <br> of diner bandits intertwine in four tales of violence and redemption.'),                                                                           
-    ('Django Unchained',10,4000,'With the help of a German bounty hunter, a freed slave sets out to rescue <br> his wife from a brutal Mississippi plantation owner.'),
-    ('Once upon a time in hollywood',10,5000,'A faded television actor and his stunt double strive to achieve fame and success in the film <br> industry during the final years of Hollywood's Golden Age in 1969 Los Angeles.');
+    ('Inglourious Bastards',10,3000,'In Nazi-occupied France during World War II, a plan to assassinate Nazi leaders by a group of <br> Jewish U.S. soldiers coincides with a theatre owners vengeful plans for the same.',0),
+    ('Kill Bill',10,2000,'After awakening from a four-year coma, a former <br> assassin wreaks vengeance on the team of assassins who betrayed her.',0),
+    ('Pulp Fiction',10,1000,'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair <br> of diner bandits intertwine in four tales of violence and redemption.',0),                                                                           
+    ('Django Unchained',10,4000,'With the help of a German bounty hunter, a freed slave sets out to rescue <br> his wife from a brutal Mississippi plantation owner.',0),
+    ('Once upon a time in hollywood',10,5000,'A faded television actor and his stunt double strive to achieve fame and success in the film <br> industry during the final years of Hollywood's Golden Age in 1969 Los Angeles.',0);
     
 INSERT INTO `Pictures`(`picture`, `productID`, `description`,`link`) 
 VALUES 
